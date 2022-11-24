6ef28604c765610f2564907d33b89ab3e974d0cf
Ahmed Raza <bhaia9036@gmail.com>
2022-11-24 05:48:54 -0800

    initial commit

--- src/header.jsx
+++ src/header.jsx
@@ -1,21 +1,77 @@
-import React, { useEffect, useRef } from 'react'
-
+import React, { useEffect, useRef, useState } from 'react'
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 const Header = () => {
     const first = useRef();
-    const two = useRef();
-    const three = useRef();
+
+// Here  is the Yup for Validation //
+
+const validationSchema = Yup.object({
+  name: Yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: Yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'), 
+    
+});
+
+
+    const formik = useFormik({
+      initialValues: {
+      name: '',
+        password: '',
+      
+      },
+      onSubmit: (values) => {
+       console.log(values)
+      },
+    });
+
 useEffect( ()=>{
-    console.log(first)
-    console.log(two)
-    console.log(three)
+
+console.log(first)
 }, []
 )
     
   return (
     <div>
-      <input ref={first}   type="text" />
-      <input ref={two}   type="text" />
-      <input ref={three}   type="text" />
+
+<form  onSubmit={formik.handleSubmit} >
+      <label>Name: </label>
+      <input  name='name'  
+      onChange={formik.handleChange}
+      value={formik.values.name}  
+      type="text" /> <br />
+{/* Error Code */}
+{formik.errors.name && formik.touched.name 
+?<p className="tr">{formik.errors.name}</p>: null }
+      <label>Password: </label>
+
+      <input  onChange={formik.handleChange} 
+       name='password'  
+       value={formik.values.password}
+       type="text" /> <br />
+       {/* Error Code */}
+{formik.errors.password && formik.touched.password 
+?<p className="tr">{formik.errors.password}</p>: null }
+
+
+      <label >Profile Photo: </label>
+
+      <input 
+       name='profilephoto'  
+       onChange={(e)=> 
+        formik.setFieldValue(" profilephoto", e.target.files[0]) } 
+       value={formik.values.profilephoto}
+       type="file" /> <br />
+          {/* Error Code */}
+{formik.errors.profilephoto && formik.touched.profilephoto 
+?<p className="tr">{formik.errors.profilephoto}</p>: null }
+<button  type='submit' >Submit</button>
+</form>
     </div>
   )
 }
