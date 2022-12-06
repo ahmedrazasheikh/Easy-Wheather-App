021b9cc146656a897e26baaac0c95401994dea86
Ahmed Raza <bhaia9036@gmail.com>
2022-12-06 01:10:28 -0800

    scond commit

--- server.mjs
+++ server.mjs
@@ -23,7 +23,8 @@ app.get('/time', (req, res) => {
 })
 
 const __dirname = path.resolve();
-app.use('/', express.static(path.join(__dirname, './build')))
+console.log(__dirname)
+// app.use('/', express.static(path.join(__dirname, './build')))
 app.use('*', express.static(path.join(__dirname, './build')))
 
 
